85129ab2e6bf05efc0da01c270ef63ab79b53e94
NIck <nick7zmail@gmail.com>
2018-11-22 19:17:52 +0500

    lookup fix
    Добавлено оформление модуля

--- modules/dev_sonoff/dev_sonoff_devices_search.inc.php
+++ modules/dev_sonoff/dev_sonoff_devices_search.inc.php
@@ -20,10 +20,14 @@
   $out['SORTBY']=$sortby_dev_sonoff_devices;
   // SEARCH RESULTS
   $res=SQLSelect("SELECT * FROM dev_sonoff_devices WHERE $qry ORDER BY ".$sortby_dev_sonoff_devices);
+  $res_online=SQLSelect("SELECT DEVICE_ID, VALUE FROM dev_sonoff_data WHERE TITLE='online'");
   if ($res[0]['ID']) {
    //paging($res, 100, $out); // search result paging
    $total=count($res);
    for($i=0;$i<$total;$i++) {
+	foreach($res_online as $id_online){
+		if($res[$i]['ID']==$id_online['DEVICE_ID'] && $id_online['VALUE']==1) $res[$i]['ONLINE']='1';   
+	}
     // some action for every record if required
     $tmp=explode(' ', $res[$i]['UPDATED']);
     $res[$i]['UPDATED']=fromDBDate($tmp[0])." ".$tmp[1];
