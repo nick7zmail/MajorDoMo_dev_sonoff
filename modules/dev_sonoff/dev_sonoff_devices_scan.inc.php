c23f69e680797cad4d8dd34103fdf5160552d3bb
NIck <nick7zmail@gmail.com>
2019-01-31 20:18:16 +0500

    sg fix

--- modules/dev_sonoff/dev_sonoff_devices_scan.inc.php
+++ modules/dev_sonoff/dev_sonoff_devices_scan.inc.php
@@ -48,7 +48,7 @@ if(!$decoded_res['error']){
 					$need_insert=false;
 					$rec_params['ID']=$findparam['ID'];
 					if(isset($findparam['LINKED_OBJECT']) && isset($findparam['LINKED_PROPERTY'])) {
-						sg($findparam['LINKED_OBJECT'].'.'.$findparam['LINKED_PROPERTY'], $this->metricsModify($param, $val, 'from_device'));
+						sg($findparam['LINKED_OBJECT'].'.'.$findparam['LINKED_PROPERTY'], $this->metricsModify($param, $val, 'from_device'), array($this->name => '0'));
 					}
 				}
 			}
@@ -70,7 +70,7 @@ if(!$decoded_res['error']){
 							$need_insert=false;
 							$rec_params['ID']=$findparam['ID'];
 							if(isset($findparam['LINKED_OBJECT']) && isset($findparam['LINKED_PROPERTY'])) {
-								sg($findparam['LINKED_OBJECT'].'.'.$findparam['LINKED_PROPERTY'], $this->metricsModify($param, $val, 'from_device'));
+								sg($findparam['LINKED_OBJECT'].'.'.$findparam['LINKED_PROPERTY'], $this->metricsModify($param, $val, 'from_device'), array($this->name => '0'));
 							}
 						}
 					}
