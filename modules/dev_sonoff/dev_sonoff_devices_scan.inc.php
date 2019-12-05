d102fcb11dcb0dde4e5897d0ed03fec706b06a25
NIck <nick7zmail@gmail.com>
2019-12-05 19:38:31 +0500

    get devices fix

--- modules/dev_sonoff/dev_sonoff_devices_scan.inc.php
+++ modules/dev_sonoff/dev_sonoff_devices_scan.inc.php
@@ -1,6 +1,35 @@
 <?php
 $this->getConfig();
-$host='https://'.$this->config['HTTPS_API_URL'].':8080/api/user/device';
+
+	//содержание файла, идущего со старыми версиями ewelink
+	$appid_str="204,208,176,196,204,176,216,192,176,224,176,220,176,200,212,176,228,176,200,196,176,204,192,176,204,196,176,204,204,176,208,224,176,208,192,176,216,196,176,200,192,176,212,228,176,204,228,176,196,228,176,216,176,204,224,176,196,208,176,196,228,176,200,220,176,208,192,176,204,228,176,216,176,212,200,176,200,220,176,208,192,176,216,208,176,212,196,176,204,216 "; //app ID
+	$key_str="216,200,176,212,200,176,208,212,176,200,220,176,204,204,176,200,204,176,208,204,176,208,216,176,216,204,176,204,224,176,216,204,176,204,216,176,196,200,176,208,204,176,212,212,176,196,208,176,200,212,176,204,196,176,204,216,176,208,192,176,204,220,176,200,200,176,220,176,200,212,176,204,192,176,204,224,176,216,196,176,216,196,176,204,192,176,212,228,176,208,196,176,212,196";//ключ
+	$dict_str='ab!@#$ijklmcdefghBCWXYZ01234DEFGHnopqrstuvwxyzAIJKLMNOPQRSTUV5689%^&*()';//словарь
+	//бъем на массивы
+	$app_arr=explode(',', $appid_str);
+	$key_arr=explode(',', $key_str);
+	$dict_arr=str_split($dict_str);	
+	//сдвигаем биты
+	foreach($key_arr as $key=>$byte) {
+		$key_arr[$key]=($byte >> 2);
+	}
+	foreach($app_arr as $key=>$byte) {
+		$app_arr[$key]=($byte >> 2);
+	}
+	//ещё пару преобразований
+    $indexes_str = implode(array_map("chr", $key_arr));
+    $indexes_arr = explode(',', $indexes_str);
+    $indexes2_str = implode(array_map("chr", $app_arr));
+    $indexes2_arr = explode(',', $indexes2_str);
+	//ищем индексы в словаре
+	foreach($indexes_arr as $index) {$crypt_key.= $dict_arr[$index];}
+	foreach($indexes2_arr as $index) {$appid.= $dict_arr[$index];}	
+	include_once("./lib/websockets/sonoffws.class.php");
+	$sonoffws = new SonoffWS($wssurl, $config);
+	$nonce=$sonoffws->generateKey(8, false);
+	
+//. '?lang=en&apiKey=' . $this->config['APIKEY'] . '&version=' . $this->config['VERSION'] . '&ts=' . time() . '&nonce=' . $this->sonoffws->generateKey(8, false) . '&appid=' . $appid . '&os=' . $this->config['OS'] . '&model=' . $this->config['MODEL'] . '&romVersion=' . $this->config['ROMVERSION'] . '&appVersion=' . $this->config['APKVERSION']
+$host='https://'.$this->config['HTTPS_API_URL'].':8080/api/user/device'. '?lang=en&apiKey=' . $this->config['APIKEY'] . '&version=' . $this->config['VERSION'] . '&ts=' . time() . '&nonce=' . $nonce . '&appid=' . $appid . '&os=' . $this->config['OS'] . '&model=' . $this->config['MODEL'] . '&romVersion=' . $this->config['ROMVERSION'] . '&appVersion=' . $this->config['APKVERSION'];
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $host);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -16,7 +45,7 @@ curl_close($ch);
 if($this->config['DEBUG']) debmes('[http] +++ '.$response, 'cycle_dev_sonoff_debug');
 $decoded_res=json_decode($response, TRUE);
 if(!$decoded_res['error']){
-	foreach($decoded_res as $device) {
+	foreach($decoded_res['devicelist'] as $device) {
 		$this->config['APIKEY']=$device['apikey'];
 		$rec['TITLE']=$device['name'];
 		$rec['DEVICEID']=$device['deviceid'];
